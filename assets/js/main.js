f56146a9be29602000b4518e769038867ad6968e
chaitalimerai <chaitalimerai143@gmail.com>
2021-01-18 20:11:57 +0530

    profile and profile edit page created

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// sidebar toggle 
+function toggleSidebar() {
+	$(".primary_nav").toggleClass("toggleActive");
+}
+function toggleSecondarySidebar() {
+	$("#secondary_nav").toggleClass("toggleActive");
+}
+
+
